723ab965d655fac14affb7148d8d3929b1d8a690
jinwoooo1150-cell <jinwoooo1150@gmail.com>
2026-02-22 01:04:04 +0900

    Fix malformed related exam statement text in quiz data

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,9 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ["dist/*"],
-  }
+    ignores: ['dist/*'],
+    rules: {
+      'import/namespace': 'off',
+    },
+  },
 ]);
